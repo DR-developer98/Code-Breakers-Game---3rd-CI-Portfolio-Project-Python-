0e3af53b5ee0753202a8686984c1ac099b58d801
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-04-26 14:03:52 +0200

    Add a function for the validation of the entered username (no blank spaces and at least 5 characters)

--- run.py
+++ run.py
@@ -17,7 +17,25 @@ def start_game():
     print("Use this feedback to adjust your strategy.")
     print("Have fun and happy code breaking!")
     print("--------------------------------------")
-    username = input("Think of a cool username of at least five characters and enter it here: \n")
-    print(f"The entered username is {username}")
+    while True:
+        try:
+            entered_username = input("Choose a username (at least 5 characters): \n")
+            if validate_username(entered_username):
+                print("The entered username is valid!")
+                break
+        except ValueError as e:
+            print(f"Error: {e}")
+    return entered_username
 
+def validate_username(username):
+    """
+    Checks for right number of digits in username
+    """
+    if " " in username:
+        raise ValueError("The username may not contain blank spaces, use dashes or underscores instead")
+    elif len(username) <5:
+        raise ValueError(f"The username must contain at least 5 characters, you entered {len(username)}")
+    else:
+        return True
+        
 start_game()
