159ee60dce1aee6979c112b7ed659dd4c7d71901
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-04-26 14:27:11 +0200

    Define function for mode choosing menu and another when to check for correct mode number

--- run.py
+++ run.py
@@ -37,5 +37,32 @@ def validate_username(username):
         raise ValueError(f"The username must contain at least 5 characters, you entered {len(username)}")
     else:
         return True
+
+def choose_mode(username):
+    """
+    Presents user with a mode-choosing menu.
+    """
+    print(f"Hi, {username}, how hard will you be challenging yourself today?")
+    print("Choose mode:\n")
+    print("1. Easy (4-digit code)\n")
+    print("2. Medium (6-digit code)\n")
+    print("3. Hard (10-digit code)\n")
+    while True:
+        entered_mode = int(input("Please enter 1, 2 or 3 for the desired mode: \n"))
+        try:
+            if validate_mode(entered_mode):
+                print("Entered mode is valid!")
+                break
+        except ValueError as e:
+            print(f"Error: {e}")
+    return entered_mode
+                
+def validate_mode(mode):
+    if mode not in [1, 2, 3]:
+        raise ValueError("Invalid input. Please enter 1, 2 or 3")
+    else:
+        return True
         
-start_game()
+chosen_username = start_game()
+selected_mode = choose_mode(chosen_username)
+
